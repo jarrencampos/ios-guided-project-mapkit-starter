0fb1c965540bb72ba11d7cfd38c96b05d35908d9
Jarren <jarrenmcampos@gmail.com>
2020-07-15 20:09:51 -0700

    Added a custom image and detail view

--- Quakes/EarthquakesViewController.swift
+++ Quakes/EarthquakesViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 import MapKit
 
+extension String {
+    static let annotationReuseIdentifier = "QuakeAnnotationView"
+}
+
 class EarthquakesViewController: UIViewController {
     
     private let quakeFetcher = QuakeFetcher()
@@ -44,6 +48,8 @@ class EarthquakesViewController: UIViewController {
             userTrackingButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
             mapView.bottomAnchor.constraint(equalTo: userTrackingButton.bottomAnchor, constant: 20)
         ])
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: .annotationReuseIdentifier)
+        
         fetchQuakes()
     }
     
@@ -62,6 +68,23 @@ extension EarthquakesViewController: MKMapViewDelegate {
     
     func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
         fetchQuakes()
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let quake = annotation as? Quake else { return nil }
+        
+        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: .annotationReuseIdentifier, for: quake) as? MKMarkerAnnotationView else {
+            preconditionFailure("Missing the registered map annotation view")
+        }
+        
+        annotationView.glyphImage = #imageLiteral(resourceName: "QuakeIcon")
+        
+        annotationView.canShowCallout = true
+        let detailView = QuakeDetailView()
+        detailView.quake = quake
+        annotationView.detailCalloutAccessoryView = detailView
+        
+        return annotationView
     }
     
 }
